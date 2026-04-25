0027f869fc722d580f5cc2522ee1b9ef79d7577d
0xshubhh <mahichshubham1@gmail.com>
2026-04-25 16:45:24 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Functions
 double add(double a, double b) { return a + b; }
 double subtract(double a, double b) { return a - b; }
 double multiply(double a, double b) { return a * b; }
